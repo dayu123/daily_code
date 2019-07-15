5413ad7adbe589869c0139aa28a6ef29b4f05586
大雨 <1075014586@qq.com>
2019-07-15 20:36:56 +0800

    defineProperty

---
+++ company/objDefineProperty.js
@@ -0,0 +1,19 @@
+console.log('nihao');
+var  obj1 = {
+    _age:19
+}
+
+Object.defineProperty(obj1,'age',{
+    set : (newAge)=>{
+        obj1._age = newAge;
+        console.log('set age'); 
+    },
+
+    get :()=>{
+        console.log('get age');
+        return obj1._age;
+    }
+
+})
+
+
