94b80a7d4727e33d07fa685368454f8a3a366632
大雨 <1075014586@qq.com>
2019-07-22 20:03:58 +0800

    about this

---
+++ company/aboutthis.js
@@ -0,0 +1,29 @@
+console.log('about this');
+
+var name = 'The window';
+
+var object1 = {
+    name: 'My object',
+
+    getNameFunc: function () {
+        return function () {
+            return this.name;
+        }
+    },
+
+    getNameFunc1: () => {
+        return this.name;
+    },
+
+    getNameFunc2: function () {
+        return this.name;
+    },
+
+}
+
+// alert(object1.getNameFunc()());
+//alert(object1.getNameFunc1());
+console.log(object1.getNameFunc1.call(object1));
+console.log(object1.getNameFunc2());
+console.log(typeof object1.getNameFunc1);
+console.log(typeof object1.getNameFunc());
