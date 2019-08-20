d189612bdfef8bfdcbbfe362a7e210bbe75af5e2
dayu <1075014586@qq.com>
2019-08-20 23:48:22 +0800

    农场牛

---
+++ dormitory/farmCow/cow.js
@@ -0,0 +1,27 @@
+
+class cowBase {
+    constructor(age) {
+        this.age = age || 0;
+    }
+
+    productNewCow() {
+        if (this.age < 5 || !this.checkHp()) {
+            return null;
+        }
+
+        return new cowBase(0);
+    }
+
+    addAge() {
+        this.age++;
+    }
+
+    checkHp() {
+        return this.age < 16;
+    }
+
+
+
+
+}
+
---
+++ dormitory/farmCow/godView.js
@@ -0,0 +1,38 @@
+class godView {
+    constructor() {
+        this.cowAry = [];
+        this.init();
+    }
+
+
+    init() {
+        this.farm = new farm();
+        this.farm.getOneCow();
+        let yearNum = 20;
+        for (let index = 0; index < yearNum; index++) {
+            console.log('  -----------------------  ');
+            console.log('第', index + 1, '年');
+            this.farm.gogo();
+
+        }
+
+        console.log(this.farm.cowAry.length);
+
+
+        let liveCowAry = _.filter(this.farm.cowAry, (cow) => {
+            return cow.checkHp();
+        });
+
+        console.log('活着的牛', liveCowAry.length, liveCowAry)
+
+
+    }
+
+
+
+
+
+
+}
+
+new godView();
